22a315cd6e4417d9ea6e756139a394841a81d288
nileshbankar <nileshsbankar@gmail.com>
2021-05-30 14:47:16 +0530

    complete funtionality

--- src/context.js
+++ src/context.js
@@ -1,27 +1,59 @@
-import React, { useState, useContext, useReducer, useEffect } from 'react'
-import cartItems from './data'
-import reducer from './reducer'
+import React, { useState, useContext, useReducer, useEffect } from "react";
+import cartItems from "./data";
+import reducer from "./reducer";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-useReducer-cart-project'
-const AppContext = React.createContext()
+const url = "https://course-api.com/react-useReducer-cart-project";
+const AppContext = React.createContext();
+
+const defaultValues = {
+  isLoading: false,
+  cart: cartItems,
+  total: 0,
+  amount: 0,
+};
 
 const AppProvider = ({ children }) => {
-  const [cart, setCart] = useState(cartItems)
+  const [state, dispatch] = useReducer(reducer, defaultValues);
+
+  // Remove all item in cart
+  const handleClearCart = () => {
+    dispatch({ type: "CLEAR_CART" });
+  };
+
+  // Remove single item from cart using id
+  const handleRemoveItem = (id) => {
+    dispatch({ type: "REMOVE_SINGLE_ITEM", value: id });
+  };
+  const handleIncrease = (id) => {
+    dispatch({ type: "INCREASE_CART_QTY", value: id });
+  };
+  const handleDecrease = (id) => {
+    dispatch({ type: "DECREASE_CART_QTY", value: id });
+  };
+
+  // Get Amount whenever cart item changes
+  useEffect(() => {
+    dispatch({ type: "GET_TOTAL" });
+  }, [state.cart]);
 
   return (
     <AppContext.Provider
       value={{
-        cart,
+        ...state,
+        handleClearCart,
+        handleRemoveItem,
+        handleIncrease,
+        handleDecrease,
       }}
     >
       {children}
     </AppContext.Provider>
-  )
-}
+  );
+};
 // make sure use
 export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
 
-export { AppContext, AppProvider }
+export { AppContext, AppProvider };
---
+++ src/reducer.js
@@ -0,0 +1,52 @@
+const reducer = (state, action) => {
+  if (action.type === "CLEAR_CART") {
+    return { ...state, cart: [] };
+  }
+  if (action.type === "REMOVE_SINGLE_ITEM") {
+    const newCart = state.cart.filter((item) => item.id !== +action.value);
+    return { ...state, cart: newCart };
+  }
+  if (action.type === "INCREASE_CART_QTY") {
+    const newCart = state.cart.map((item) => {
+      if (item.id === +action.value) {
+        return { ...item, amount: item.amount++ };
+      }
+      return item;
+    });
+    return { ...state, cart: newCart };
+  }
+  if (action.type === "DECREASE_CART_QTY") {
+    const newCart = state.cart
+      .map((item) => {
+        if (item.id === +action.value) {
+          if (item.amount === 0) return {};
+          else return { ...item, amount: item.amount-- };
+        }
+        return item;
+      })
+      .filter((item) => item.amount >= 0);
+    return { ...state, cart: newCart };
+  }
+  if (action.type === "GET_TOTAL") {
+    let cartTotal = state.cart.reduce(
+      (acc, cartItem) => {
+        acc.total += cartItem.price * cartItem.amount;
+        acc.amount += cartItem.amount;
+
+        return acc;
+      },
+      {
+        amount: 0,
+        total: 0,
+      }
+    );
+
+    return {
+      ...state,
+      total: parseFloat(cartTotal.total.toFixed(2)),
+      amount: cartTotal.amount,
+    };
+  }
+};
+
+export default reducer;
